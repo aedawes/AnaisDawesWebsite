8cf3855e43942bc7bc6db2db3e731633c663ec5e
aedawes <emme.dawes@me.com>
2024-06-05 22:21:48 -0600

    Fixes spacing bugs on homepage

--- anaisdawes/src/Pages/Projects.js
+++ anaisdawes/src/Pages/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/Navbar.js';
 import '../CSS/Projects.css';
 import Footer from '../Components/Footer.js';
@@ -6,12 +6,31 @@ import { listOfProjects } from '../database.js';
 import ProjectCard from '../Components/ProjectCard.js';
 
 function Projects() {
+    const [isSmallWindow, setIsSmallWindow] = useState(false);
     const projects = listOfProjects()
+
+    useEffect(() => {
+        function isWindowSize(size) {
+            return window.innerWidth <= size;
+        }
+
+        function handleResize() {
+            setIsSmallWindow(isWindowSize(718));
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <div className='app'>
             <Navbar />
-            <div className='pageContent'>
+            <div className={`pageContent ${isSmallWindow ? 'center' : ''}`}>
                 <h1 className='pageHeader'>Projects</h1>
                 <h2 className='pageSubheader'>Select a project to learn more</h2>
                 <div className='projectsContainer'>
